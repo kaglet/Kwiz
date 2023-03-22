120e19ff2e08347626d1733fab8c2d103bb1b7bb
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-03-22 21:38:04 +0200

    Remove imports of deleted files

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kwiz/pages/home.dart';
 import 'package:kwiz/pages/create_quiz.dart';
 
-import 'quote.dart';
-import 'quote_card.dart';
-
 void main() => runApp(MaterialApp(
       home: AddQuiz(),
     ));
--- lib/quote.dart
+++
@@ -1,6 +0,0 @@
-class Quote {
-  late String text;
-  late String author;
-
-  Quote({required this.text, required this.author});
-}
