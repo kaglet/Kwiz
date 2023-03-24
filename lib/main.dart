1b2f8bbfb52618c956ebb92e0adcd1d77f14881e
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-03-24 19:59:55 +0200

    Make group pages work independently

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
-// import 'dart:js';
-
 import 'package:flutter/material.dart';
-=======
-import 'package:kwiz/quiz_screen.dart';
->>>>>>> 177c6e4e8ad063a18aff3ac948b6fd5d87068f93
 import 'package:kwiz/pages/home.dart';
 import 'package:kwiz/pages/create_quiz.dart';
-import 'package:kwiz/pages/loading.dart';
+import 'package:kwiz/pages/viewquizzes/viewquizzes.dart';
+import 'package:kwiz/quiz_screen.dart';
+import 'package:kwiz/start_quiz.dart';
+import 'package:kwiz/view_categories.dart';
 
 void main() => runApp(MaterialApp(
-<<<<<<< HEAD
-      home: Home(),
-=======
       theme: ThemeData(
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Color.fromARGB(255, 49, 49, 49),
-        textTheme: TextTheme(
-          headline1: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-          headline2: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-          bodyText2: TextStyle(color: Color.fromARGB(255, 238, 238, 238)),
-          subtitle1: TextStyle(color: Color.fromARGB(255, 157, 199, 123)),
-        )
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+            .copyWith(
+                secondary: const Color.fromARGB(
+                    255, 138, 31, 156)), // Set the accent color to purple
       ),
-      home: AddQuiz(),
->>>>>>> 177c6e4e8ad063a18aff3ac948b6fd5d87068f93
+      home: ViewQuizzes(),
     ));
