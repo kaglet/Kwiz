843abda5aff0192dbd21b14a3e985f04bd8e17b7
Michael Kavai <mchlkavai@gmail.com>
2023-03-25 11:26:22 +0200

    Fix start_quiz renderflex overflow error

--- lib/view_categories.dart
+++ lib/view_categories.dart
@@ -1,27 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:kwiz/pages/viewquizzes/viewquizzes.dart';
+//References: https://api.flutter.dev/flutter/material/AppBar-class.html
+ //Using this array to test the dynamic aspect of the page. This will be replaced by prompts from our database eventually...
+var categories = ['All','Art','Biology','Comics','Fashion','Fiction','Food','Gaming','Entertainment','History','Holidays','Languages','Mathematics','Music','Science','Sport','Politics','Technology','Transport','+'];
 
 //References: https://api.flutter.dev/flutter/material/AppBar-class.html
 //Using this array to test the dynamic aspect of the page. This will be replaced by prompts from our database eventually...
-var categories = [
-  'All',
-  'Art',
-  'Biology',
-  'Fashion',
-  'Food',
-  'Gaming',
-  'Entertainment',
-  'History',
-  'Holidays',
-  'Languages',
-  'Mathematics',
-  'Music',
-  'Science',
-  'Sport',
-  'Politics',
-  'Technology',
-  'Transport'
-];
+
 
 void main() => runApp(const ViewCategories());
 
@@ -32,8 +17,7 @@ class ViewCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorSchemeSeed: Color.fromARGB(255, 14, 39, 179),
-        //colorSchemeSeed: Colors.blue,
+        colorSchemeSeed: Colors.white,
         useMaterial3: true,
       ),
       home: const ViewCategoriesScreen(),
@@ -59,129 +43,123 @@ class _ViewCategoriesState extends State<ViewCategoriesScreen> {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final Color oddItemColor = colorScheme.primary.withOpacity(0.10);
     final Color evenItemColor = colorScheme.primary.withOpacity(0.30);
-
     //Search bar reference: https://flutterassets.com/search-bar-in-flutter/
-
-    //Scaffold consists of: An app bar that contains the Home button and Seach box , the body that has a gridview
+    //Scaffold consists of: An app bar that contains the Home button and Search box , the body that has a gridview
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        //backgroundColor: Color.fromARGB(255, 187, 182, 182).withOpacity(0.1),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            SizedBox(
-              width: 130,
-              height: 50,
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  side: BorderSide(width: 1.0, color: Colors.black),
-                  //primary: Colors.green,
-                  textStyle: const TextStyle(
-                    fontSize: 18,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    //fontWeight: FontWeight.bold,
+      backgroundColor: Colors.black, 
+      body: Padding(
+        padding: const EdgeInsets.only( top: 30),
+        child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(
+                  width: 140,
+                  height: 70,
+                  child: ElevatedButton.icon(
+                    
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                    side: BorderSide(width:1.0, color:Colors.white),
+                    //primary: Colors.green,
+                    textStyle: const TextStyle(
+                        fontSize: 10, 
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        //fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {},
+                      icon: Icon(Icons.home, color: Colors.black,size:32,),
+                        label: const Text('Home', style: TextStyle(fontSize: 19,color: Colors.black),),
+                      ),
                   ),
-                ),
-                onPressed: () {},
-                icon: Icon(
-                  Icons.home,
-                  color: Colors.black,
-                  size: 28,
-                ),
-                label: const Text(
-                  'Home',
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                ),
-              ),
-            ),
-            //Spacer(),
-            Flexible(
-              child: SizedBox(
-                width: 280,
-                height: 50,
-                child: TextField(
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    hintText: 'Search Categories',
-                    // Add a clear button to the search bar
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        Icons.clear,
-                        color: Colors.black,
+
+                  Flexible(
+                    child: 
+                      TextField(
+                          controller: _searchController,
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                          filled: true, 
+                          fillColor: Colors.white,
+                          hintText: 'Search Categories',
+                          // Add a clear button to the search bar
+                          suffixIcon: IconButton(
+                          icon: Icon(Icons.clear, color: Colors.black,size:32),
+                          onPressed: () => _searchController.clear(),
+                          ),
+                          // Add a search icon or button to the search bar
+                            prefixIcon: IconButton(
+                          icon: Icon(Icons.search,size: 30, color: Colors.black,),
+                          onPressed: () {
+                              // Perform the search here
+                                    },
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50.0),
+                              borderSide: BorderSide(width: 15, color:Colors.white),
+                         
+                            ),
+                        ),
                       ),
-                      onPressed: () => _searchController.clear(),
-                    ),
-                    // Add a search icon or button to the search bar
-                    prefixIcon: IconButton(
-                      icon: Icon(
-                        Icons.search,
-                        size: 30,
-                        color: Colors.black,
-                      ),
-                      onPressed: () {
-                        // Perform the search here
-                      },
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                      borderSide: BorderSide(width: 12, color: Colors.black),
-                    ),
                   ),
-                ),
-              ),
+               ],
             ),
-          ],
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Flexible(
-            child: GridView.builder(
-                itemCount: categories.length,
-                padding: const EdgeInsets.all(8.0),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1,
-                  mainAxisSpacing: 10.0,
-                  crossAxisSpacing: 10.0,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  final Color color =
-                      Colors.primaries[index % Colors.primaries.length];
-
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ViewQuizzes()),
-                      );
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
-
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50.0),
-                        border: Border.all(width: 2 /*color: Colors.white*/),
-                        color: color,
-                      ),
-                      child: Text(
-                        categories[index],
+                Flexible(
+                  child: GridView.builder(
+                    itemCount: categories.length,
+                    padding: const EdgeInsets.all(8.0),
+                       
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1,
+                    mainAxisSpacing:  10.0,
+                    crossAxisSpacing: 10.0,
+                    
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                     final Color color = Colors.primaries[index % Colors.primaries.length];
+                    return GestureDetector(
+                      //This onTap is just to check if tapping on the cards works or not
+                         onTap: () {
+                              setState(() { 
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                  content: Text('You tapped on ' + categories[index] +'!'),
+                                  action: SnackBarAction(
+                                      onPressed: (){ 
+                                      },
+                                      label: "DISMISS",
+                                  ),
+                                )
+                              );
+                          });
+                        },
+                        child: Container(
+                        alignment: Alignment.center,                        
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50.0),
+                          border: Border.all(width: 2),
+                          color: color,
+                        ),
+                        child: Text(categories[index],
                         style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white.withOpacity(1.0)),
-                      ),
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white.withOpacity(1.0)
+                              ),
+                            ),       
+                          ),
+                        );
+                      }),
                     ),
-                  );
-                }),
-          ),
-        ],
-      ),
-    );
-  }
+                  ],
+                ),
+              ),
+            );
+          }
 }
