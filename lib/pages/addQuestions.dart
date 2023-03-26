ee7c5ada30360017567fa52735643651b12dd011
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-03-26 13:58:42 +0200

    Create new addQuestions screen

--- lib/pages/addQuestions.dart
+++ lib/pages/addQuestions.dart
@@ -4,60 +4,25 @@ import 'package:kwiz/classes/QA.dart';
 import 'package:kwiz/classes/QAwidget.dart';
 import 'package:kwiz/classes/aboutCard.dart';
 import 'package:kwiz/classes/multiLineTextField.dart';
-import 'package:kwiz/pages/addQuestions.dart';
 
-class AddQuiz extends StatefulWidget {
-  const AddQuiz({super.key});
+class AddQuestions extends StatefulWidget {
+  const AddQuestions({super.key});
 
   @override
-  State<AddQuiz> createState() => AddQuizState();
+  State<AddQuestions> createState() => _AddQuestionsState();
 }
 
-class AddQuizState extends State<AddQuiz> {
+class _AddQuestionsState extends State<AddQuestions> {
   List<QAContainer> QAContainers = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Color.fromARGB(255, 249, 235, 237),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Row(
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.home,
-                      color: Colors.black,
-                      size: 20.0,
-                    ),
-                    label: Text(
-                      'Home',
-                      style: TextStyle(
-                        color: Colors.black,
-                        letterSpacing: 1.0,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.red.shade400),
-                    ),
-                  ),
-                ],
-              ),
-              aboutQuizCard(),
-              Divider(
-                height: 20.0,
-                color: Color.fromARGB(255, 8, 8, 8),
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
@@ -69,11 +34,7 @@ class AddQuizState extends State<AddQuiz> {
                     child: TextButton(
                       onPressed: () {
                         setState(() {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => AddQuestions()),
-                          );
+                          QAContainers.clear();
                         });
                       },
                       style: ButtonStyle(
@@ -170,17 +131,3 @@ class AddQuizState extends State<AddQuiz> {
     );
   }
 }
-
-// class MyButton extends StatelessWidget {
-//   const MyButton({super.key});
-
-//   MyButton({required this.color, this.text});
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
-
-// create custom button class since all have the same property
-// Think in terms of flexbox don't forget everything there. The space works very well with flex properties, to make empty space in a container when padding is too hard.
-// spacing is also very customizable.
