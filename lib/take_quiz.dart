e4fcf721a9bfb2359fb2ef0ac0697223896d77ac
Christine Bau <christinebau07@gmail.com>
2023-04-06 21:10:09 +0200

    ui color changes and category images

--- lib/take_quiz.dart
+++ lib/take_quiz.dart
@@ -19,8 +19,7 @@ class _QuizScreenState extends State<QuizScreen> {
 
   List<String> userAnswers = [];
 
-  //String qID = 'TJvZqgQaVC9LkBqeVqlL';
-
+  //Getting quiz, length of quiz and populating quiz questions and answers
   Future<void> loaddata() async {
     setState(() {
       _isLoading = true;
@@ -29,6 +28,7 @@ class _QuizScreenState extends State<QuizScreen> {
     quizLength =
         quiz!.quizQuestions.length; //this seemed to have fixed the null error?
     userAnswers = List.filled(quizLength, '');
+    category = quiz!.quizCategory.toString();
     popList(quiz);
     setState(() {
       _isLoading = false;
@@ -41,6 +41,7 @@ class _QuizScreenState extends State<QuizScreen> {
     super.initState();
   }
 
+  //function for populating the questions and answers
   void popList(Quiz? q) {
     for (int i = 0; i < quizLength; i++) {
       questions.add(q!.quizQuestions.elementAt(i).questionText);
@@ -48,6 +49,7 @@ class _QuizScreenState extends State<QuizScreen> {
     }
   }
 
+  //Two local var lists for storing answers and questions
   List<String> questions = [];
   List<String> answers = [];
 
@@ -56,6 +58,8 @@ class _QuizScreenState extends State<QuizScreen> {
 
   //get quizname from firebase
   final String quizName = 'PlaceHolder :)';
+
+  String category = "";
 
   // Index of the current question
   int currentIndex = 0;
@@ -72,6 +76,7 @@ class _QuizScreenState extends State<QuizScreen> {
           ? null
           : AppBar(
               title: Text(quiz!.quizName),
+              backgroundColor: Colors.transparent,
               leading: IconButton(
               icon: const Icon(Icons.arrow_back_ios_new_outlined),
               onPressed: () {
@@ -85,7 +90,19 @@ class _QuizScreenState extends State<QuizScreen> {
                 child: CircularProgressIndicator(),
               )
             //after data is loaded this displays
-            :Padding(
+            : Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 27, 57, 82),
+                  Color.fromARGB(255, 5, 12, 31),
+                ],
+              ),
+              ),
+
+            child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -93,7 +110,11 @@ class _QuizScreenState extends State<QuizScreen> {
                     Flexible(
                       flex: 1,
                       fit: FlexFit.tight,
-                      child: Image.asset('assets/images/quizBg.jpg'),
+                      child: Center( 
+                        child: Image.asset(
+                        'assets/images/$category.gif', 
+                        )
+                      )
                     ),
 
                     // Display the question number and question text
@@ -111,6 +132,7 @@ class _QuizScreenState extends State<QuizScreen> {
                           fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
                     ),
                     const SizedBox(height: 32.0),
+                    
                     // Input field for the answer
 
                     TextField(
@@ -123,39 +145,78 @@ class _QuizScreenState extends State<QuizScreen> {
                     ),
 
                     const SizedBox(height: 32.0),
+
                     // Buttons for moving to the previous/next question
+                    
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         if (currentIndex > 0)
-                          ElevatedButton(
+                        Container(
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [Colors.blue, Colors.blue],
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          child: ElevatedButton(
                             onPressed: () {
                               setState(() {
                                 currentIndex--;
                                 updateText();
-                                //print(currentIndex);
-                                //answerController.clear();
                               });
                             },
-                            child: const Text('Previous'),
+                            style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                            primary: Colors.transparent, // set the button background color to transparent
+                            elevation: 0, // remove the button shadow
                           ),
+                            child: const Text('Previous'),
+                        ),
+                        ),
+
                         if (currentIndex < questions.length - 1)
-                          ElevatedButton(
+                        Container(
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [Colors.orange, Colors.orange],
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          child: ElevatedButton(
                             onPressed: () {
                               userAnswers[currentIndex] =
                                   answerController.text.trim();
                               setState(() {
                                 currentIndex++;
                                 updateText();
-                                //print(currentIndex);
-                                //answerController.clear();
                               });
                             },
-                            child: const Text('Next'),
+                            style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                            primary: Colors.transparent, // set the button background color to transparent
+                            elevation: 0, // remove the button shadow
                           ),
+                            child: const Text('  Next  '),
+                        ),
+                        ),
+
                         // Show a submit button on the last question
                         if (currentIndex == questions.length - 1)
-                          ElevatedButton(
+                          Container(
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [Colors.orange, Colors.deepOrange],
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          child: ElevatedButton(
                             onPressed: () {
                               // Calculate the score
                               userAnswers[currentIndex] = answerController.text.trim();
@@ -214,248 +275,23 @@ class _QuizScreenState extends State<QuizScreen> {
                               }
                             },
                             child: const Text('Submit'),
+                            style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                            primary: Colors.transparent, // set the button background color to transparent
+                            elevation: 0, // remove the button shadow
                           ),
+                        ),
+                        ),
                       ],
                     ),
                   ],
                 ),
               ),
       ),
+      ),
     );
   }
 }
-// import 'dart:io';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-// import 'package:kwiz/hello.dart';
-// import 'package:kwiz/firebase_options.dart';
-// import 'package:kwiz/services/database.dart';
-
-// import 'Models/Quizzes.dart';
-
-// class QuizScreen extends StatefulWidget {
-//   @override
-//   _QuizScreenState createState() => _QuizScreenState();
-// }
-
-// class _QuizScreenState extends State<QuizScreen> {
-//   DatabaseService service = DatabaseService();
-//   // Get the questions from firebase
-
-//   Quiz? quiz;
-//   late bool _isLoading;
-
-//   int quizLength = 0;
-
-//   Future<void> loaddata() async {
-//     setState(() {
-//       _isLoading = true;
-//     });
-//     try {
-//       quiz = await service.getQuizAndQuestions(QuizID: 'IXB6iGJugaXnMDc1rwdn');
-
-//       quizLength = quiz!
-//           .QuizQuestions.length; //this seemed to have fixed the null error?
-//       popList(quiz);
-//     } catch (e) {
-//       print('$e');
-//     }
-//     setState(() {
-//       _isLoading = false;
-//     });
-//   }
-
-//   @override
-//   void initState() {
-//     loaddata();
-//     super.initState();
-//   }
-
-//   void popList(Quiz? q) {
-//     for (int i = 0; i < quizLength; i++) {
-//       questions[i] = (q!.QuizQuestions.elementAt(i).QuestionText);
-//       answers[i] = (q!.QuizQuestions.elementAt(i).QuestionAnswer);
-//     }
-//     // for (int i = 0; i < quizLength; i++) {
-//     //   questions.add(q!.QuizQuestions.elementAt(i).QuestionText);
-//     //   answers.add(q!.QuizQuestions.elementAt(i).QuestionAnswer);
-//     // }
-//   }
-
-//   // void popList(Quiz? q){
-//   //     for (int i = 0; i <quizLength; i++){
-//   //       questions.add(q!.QuizQuestions.elementAt(i).QuestionText);
-//   //       answers.add(q.QuizQuestions.elementAt(i).QuestionAnswer);
-//   //     }
-//   //   }
-
-//   List<String> questions = [];
-//   List<String> answers = [];
-
-//   // Controller for the answer input field
-//   TextEditingController answerController = TextEditingController();
-
-//   //user input answers
-//   List<String> userAnswers = List.filled(3, ''); //make this dynamic!!!
-
-//   //get quizname from firebase
-//   final String quizName = 'PlaceHolder :)';
-
-//   // Index of the current question
-//   int currentIndex = 0;
-
-//   // final Future<Quiz?> quiz = loaddata();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     void updateText() {
-//       answerController.text = userAnswers[currentIndex];
-//     }
-
-//     // questions = List.filled(3, ''); //the hardcoded works but crashes
-//     // answers = List.filled(3, ''); //initialise size of answer and question list
-
-//     // popList(quiz);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//             quizName), // save the title as a global variabe thats pulled from firebase as well
-//       ),
-//       body: _isLoading
-//           ? const Center(
-//               child: CircularProgressIndicator(),
-//             )
-//           : Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Flexible(
-//                     flex: 1,
-//                     fit: FlexFit.tight,
-//                     child: Image.asset('assets/images/geo1.jpg'),
-//                   ),
-//                   // Display the question number and question text
-
-//                   TextButton(
-//                     onPressed: () {
-//                       print(quiz!.QuizQuestions.elementAt(1).QuestionText);
-//                       print(currentIndex);
-//                     },
-//                     child: Text('Get Quiz with Questions'),
-//                   ),
-
-//                   Text(
-//                     'Question ${currentIndex + 1} of ${questions.length}', //
-//                     style:
-//                         TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-//                   ),
-//                   SizedBox(height: 16.0),
-//                   Text(
-//                     questions[currentIndex],
-//                     style:
-//                         TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-//                   ),
-//                   SizedBox(height: 32.0),
-//                   // Input field for the answer
-
-//                   TextField(
-//                     controller: answerController,
-//                     decoration: InputDecoration(
-//                       hintText: 'Type your answer here',
-//                       hintStyle:
-//                           TextStyle(color: Color.fromARGB(255, 107, 106, 106)),
-//                     ),
-//                   ),
-
-//                   SizedBox(height: 32.0),
-//                   // Buttons for moving to the previous/next question
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: [
-//                       if (currentIndex > 0)
-//                         ElevatedButton(
-//                           onPressed: () {
-//                             setState(() {
-//                               currentIndex--;
-//                               updateText();
-//                               //print(currentIndex);
-//                               //answerController.clear();
-//                             });
-//                           },
-//                           child: Text('Previous'),
-//                         ),
-//                       if (currentIndex < questions.length - 1)
-//                         ElevatedButton(
-//                           onPressed: () {
-//                             userAnswers[currentIndex] =
-//                                 answerController.text.trim();
-//                             /////userAnswers.add(answerController.text.trim());
-//                             // print(userAnswers);
-//                             setState(() {
-//                               currentIndex++;
-//                               updateText();
-//                               //print(currentIndex);
-//                               //answerController.clear();
-//                             });
-//                           },
-//                           child: Text('Next'),
-//                         ),
-//                       // Show a submit button on the last question
-//                       if (currentIndex == questions.length - 1)
-//                         ElevatedButton(
-//                           onPressed: () {
-//                             // Calculate the score
-
-//                             userAnswers[currentIndex] =
-//                                 answerController.text.trim();
-//                             int score = 0;
-//                             for (int i = 0; i < questions.length; i++) {
-//                               if (userAnswers[i].toLowerCase() ==
-//                                   answers[i].toLowerCase()) {
-//                                 score++;
-//                                 //print(answerController.text);
-//                               }
-//                             }
-//                             // Show the score in an alert dialog
-
-//                             showDialog(
-//                               context: context,
-//                               builder: (BuildContext context) {
-//                                 return AlertDialog(
-//                                   title: Text('Quiz Complete'),
-//                                   content: Text(
-//                                       'Your score: $score / ${questions.length}'),
-//                                   actions: [
-//                                     TextButton(
-//                                       onPressed: () {
-//                                         Navigator.of(context).pop();
-//                                       },
-//                                       child: Text('OK'),
-//                                     ),
-//                                   ],
-//                                 );
-//                               },
-//                             );
-
-//                             // //TESTING MOVING FROM SCREEN TO SCREEN
-//                             // Navigator.push(
-//                             //   context,
-//                             //   MaterialPageRoute(builder: (context) => HelloPage()),
-//                             //  );
-//                           },
-//                           child: Text('Submit'),
-//                         ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//     );
-//   }
-// }
 
 // //USE WIDGET INSPECTOR type Ctrl + T and search >Dart: Open DevTools
 // //Flex makes it "become small" can be squashed :) the picture of the globe is squashed when we click on input box.
