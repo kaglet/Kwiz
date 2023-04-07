05199af4dba4f83dade0209bbdf6d072bc75fea8
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-07 11:54:25 +0200

    Add widget test for more code coverage

--- test/fake_test.dart
+++ test/fake_test.dart
@@ -1,8 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kwiz/pages/home.dart';
+import 'package:kwiz/pages/add_quiz_about.dart';
+import 'package:kwiz/view_categories.dart';
 
 void main() {
-  // run a fake test
-  test('Fake test', () {
-    expect(1, 1);
+  testWidgets('Home screen buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Home()));
+
+    expect(find.text('Browse our quizzes'), findsOneWidget);
+    expect(find.text('Add custom quiz'), findsOneWidget);
+
+    await tester.tap(find.text('Browse our quizzes'));
+    await tester.pumpAndSettle();
+    expect(find.byType(ViewCategories), findsOneWidget);
+
+    await tester.tap(find.text('Add custom quiz'));
+    await tester.pumpAndSettle();
+    expect(find.byType(AddQuiz), findsOneWidget);
   });
 }
