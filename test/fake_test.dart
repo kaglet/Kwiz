cc25ffec43808b960161b25ca38482bcce3aa99b
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-07 14:12:07 +0200

    Revert test

--- test/fake_test.dart
+++ test/fake_test.dart
@@ -1,22 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kwiz/pages/home.dart';
-import 'package:kwiz/pages/add_quiz_about.dart';
-import 'package:kwiz/view_categories.dart';
 
 void main() {
-  testWidgets('Home screen buttons', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: Home()));
-
-    expect(find.text('Browse our quizzes'), findsOneWidget);
-    expect(find.text('Add custom quiz'), findsOneWidget);
-
-    await tester.tap(find.text('Browse our quizzes'));
-    await tester.pumpAndSettle();
-    expect(find.byType(ViewCategories), findsOneWidget);
-
-    await tester.tap(find.text('Add custom quiz'));
-    await tester.pumpAndSettle();
-    expect(find.byType(AddQuiz), findsOneWidget);
+  // run a fake test
+  test('Fake test', () {
+    expect(1, 1);
   });
 }
